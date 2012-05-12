0dcb217609fb27bb20aa0f19d8ccddad7679095b
Brian Michel <brian.michel@gmail.com>
2012-05-12 17:12:47 -0400

    moved over to lib/spotify

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -1,5 +1,15 @@
+SPOTIFY_API_VERSION = 1
 module Muzak
   class Spotify
-    
+  	include HTTParty
+  	base_uri 'ws.spotify.com'
+
+	def self.track_id_from_term(term)
+		call_service_for_term(term.gsub!(' ', '+'))
+	end
+private
+	def call_service_for_term(term)
+		self.get("/search/#{SPOTIFY_API_VERSION}/track.json?q=#{term}")
+ 	end	
   end  
 end
