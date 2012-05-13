87638b0cdee199b5bfacfcecb0a0d00ed83c84d6
brookemckim <brooke.mckim@gmail.com>
2012-05-12 20:10:51 -0400

    refactor

--- lib/echonest.rb
+++ lib/echonest.rb
@@ -2,60 +2,75 @@ require 'httparty'
 require 'json'
 
 module Muzak
-  class EchoNest
-    class ApiRequestError < StandardError; end;
+  module EchoNest
+    def self.new(*args)
+      Client.new(*args)
+    end  
     
     class Song
-      attr_accessor :artist_id, :id, :artist_name, :title
+      attr_reader :artist_id, :id, :artist_name, :title
       
       def initialize(args = {})
         args.each do |k,v|
-          self.send("#{k}=", v)
-        end  
+          self.instance_variable_set("@#{k}", v)
+        end
       end  
-    end  
-    
-    BASE_URL = 'http://developer.echonest.com/api/v4/'
-    
-    def initialize(api_key)
-      @api_key = api_key
-    end  
-    
-    def terms(type = :style)
-      type = type.to_sym
-      
-      resp  = get 'artist/list_terms', {type: type}
-      terms = resp['response']['terms']
-            
-      terms.collect { |t| t['name'] }
-    rescue ApiRequestError
-      []  
     end
     
-    def song_by_mood(mood)
-      resp  = get 'song/search', {mood: mood}
-      songs = resp['response']['songs']
-      
-      songs.collect { |s| Song.new s }
-    rescue ApiRequestError
-      []  
+    class Term
+      attr_reader :name
+          
+      def initialize(args = {})
+        args.each do |k,v|
+          self.instance_variable_set("@#{k}", v)
+        end  
+      end  
     end    
     
-    private 
+    class Client
+      class ApiRequestError < StandardError; end;
+          
+      BASE_URL = 'http://developer.echonest.com/api/v4/'
     
-    def get(node, params = {})
-      params.merge!({api_key: @api_key, format: 'json'})
+      def initialize(api_key)
+        @api_key = api_key
+      end  
+    
+      def terms(type = :style)
+        type = type.to_sym
+      
+        resp  = get 'artist/list_terms', {type: type}
+        terms = resp['response']['terms']
             
-      url  = BASE_URL + node   
-      url  = url + '?' + params.collect { |k,v| "#{k}=#{v}" }.join('&') 
-       
-      resp = HTTParty.get(url)
-            
-      if resp.response.code == '200'
-        JSON.parse resp.body
-      else
-        raise ApiRequestError
+        terms.collect { |t| Term.new t }
+      rescue ApiRequestError
+        []  
+      end
+    
+      def songs_for_mood(mood)
+        resp  = get 'song/search', {mood: mood}
+        songs = resp['response']['songs']
+      
+        songs.collect { |s| Song.new s }
+      rescue ApiRequestError
+        []  
       end    
-    end  
+    
+      private 
+    
+      def get(node, params = {})
+        params.merge!({api_key: @api_key, format: 'json'})
+            
+        url  = BASE_URL + node   
+        url  = url + '?' + params.collect { |k,v| "#{k}=#{v}" }.join('&') 
+        resp = HTTParty.get(url)
+            
+        if resp.response.code == '200'
+          JSON.parse resp.body
+        else
+          raise ApiRequestError
+        end    
+      end 
+    end   
   end  
 end
