393f5e0cb700ace3c4adcdfd7641d99c2c54ee87
Brian Michel <brian.michel@gmail.com>
2012-05-14 18:41:48 -0400

    update spotify client

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -3,16 +3,20 @@ require 'json'
 
 SPOTIFY_API_VERSION = 1
 module Muzak
-  class Spotify
+	class Spotify
   	include HTTParty
   	base_uri 'ws.spotify.com'
 
-	def self.track_id_from_term(term)
-		Spotify.call_service_for_term(term.gsub!(' ', '+'))
-	end
+		def self.track_id_from_term(term)
+			Spotify.call_service_for_term(term.gsub!(' ', '+'))
+		end
 private
-	def self.call_service_for_term(term)
-		 self.get("/search/#{SPOTIFY_API_VERSION}/track.json?q=#{term}").body
- 	end	
+		def self.call_service_for_term(term)
+			resp = self.get("/search/#{SPOTIFY_API_VERSION}/track.json?q=#{term}")
+			if resp.code == 200
+		 		body = JSON.parse resp.body
+		 		body['tracks'].collect {|track| track['href']}.join("\n")
+		 end
+ 		end	
   end  
 end
